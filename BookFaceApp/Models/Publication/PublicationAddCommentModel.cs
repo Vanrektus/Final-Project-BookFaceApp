c30e36b4f0e86f3baf3fd5c1d0ea6e2fe5fe414b
Vanrektus <mumiata123@abv.bg>
2022-10-29 20:42:34 +0300

    Added publication details/edit/comments/likes functionality

---
+++ BookFaceApp/Models/Publication/PublicationAddCommentModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static BookFaceApp.Data.DataConstants.CommentConstants;
+
+namespace BookFaceApp.Models.Publication
+{
+    public class PublicationAddCommentModel
+    {
+        [Required]
+        [MaxLength(MaxCommentText)]
+        public string Text { get; set; } = null!;
+
+        public int PublicationId { get; set; }
+    }
+}
---
+++ BookFaceApp/Models/Publication/PublicationEditModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static BookFaceApp.Data.DataConstants.PublicationConstants;
+
+namespace BookFaceApp.Models.Publication
+{
+    public class PublicationEditModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(MaxPublicationTitle)]
+        public string Title { get; set; } = null!;
+
+        public string? ImageUrl { get; set; }
+
+        public string? UserId { get; set; } = null!;
+    }
+}
