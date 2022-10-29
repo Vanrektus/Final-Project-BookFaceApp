b73a954c1acf4e29442151c0e54dcabc6d6374f7
Vanrektus <mumiata123@abv.bg>
2022-10-29 20:36:30 +0300

    Added registration/login/logout functionality

--- BookFaceApp/Controllers/HomeController.cs
+++ BookFaceApp/Controllers/HomeController.cs
@@ -6,27 +6,14 @@ namespace BookFaceApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
-        }
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("All", "Publication");
+            }
 
-        public IActionResult Privacy()
-        {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ BookFaceApp/Models/User/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static BookFaceApp.Data.DataConstants.UserConstants;
+
+namespace BookFaceApp.Models.User
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(MaxUserUserName, MinimumLength = MinUserUserName)]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(MaxUserEmail, MinimumLength = MinUserEmail)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(MaxUserPassword, MinimumLength = MinUserPassword)]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
