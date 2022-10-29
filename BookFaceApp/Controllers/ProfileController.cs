18b7b85b274a7372f9df9db31212ce39f2bef850
Vanrektus <mumiata123@abv.bg>
2022-10-29 20:44:00 +0300

    Added my profile page

---
+++ BookFaceApp/Controllers/ProfileController.cs
@@ -0,0 +1,28 @@
+﻿using BookFaceApp.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BookFaceApp.Controllers
+{
+    [Authorize]
+    public class ProfileController : Controller
+    {
+        private readonly IPublicationService publicationService;
+
+        public ProfileController(IPublicationService _publicationService)
+        {
+            publicationService = _publicationService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userId = User.Claims
+                .FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            var model = await publicationService.GetMineAsync(userId!);
+
+            return View(model);
+        }
+    }
+}
