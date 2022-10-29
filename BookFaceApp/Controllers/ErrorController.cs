a3dcaa56ebb6294c9483211f58c93c88f17950b2
Vanrektus <mumiata123@abv.bg>
2022-10-29 20:46:07 +0300

    Added error page

---
+++ BookFaceApp/Controllers/ErrorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookFaceApp.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult NotOwner()
+        {
+            return View();
+        }
+    }
+}
