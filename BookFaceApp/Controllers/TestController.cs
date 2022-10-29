d9ae95db2df07035e49b96d50cd0af64500dc078
Vanrektus <mumiata123@abv.bg>
2022-10-29 20:39:17 +0300

    Added test page

---
+++ BookFaceApp/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookFaceApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
