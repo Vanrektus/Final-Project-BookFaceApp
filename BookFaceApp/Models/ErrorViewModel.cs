179d41e1f82157182be3f87dfefbec9cf57590a4
Vanrektus <mumiata123@abv.bg>
2022-10-29 20:17:30 +0300

    Add project files.

---
+++ BookFaceApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookFaceApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
