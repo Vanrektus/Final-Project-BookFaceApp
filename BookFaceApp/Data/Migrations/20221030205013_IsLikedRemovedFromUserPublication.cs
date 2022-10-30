5a23ecc463fe4350be38eae264610d33a04a554d
Vanrektus <mumiata123@abv.bg>
2022-10-30 22:52:07 +0200

    isLiked table removed from UserPublication migration

---
+++ BookFaceApp/Data/Migrations/20221030205013_IsLikedRemovedFromUserPublication.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookFaceApp.Data.Migrations
+{
+    public partial class IsLikedRemovedFromUserPublication : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isLiked",
+                table: "UsersPublications");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isLiked",
+                table: "UsersPublications",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
